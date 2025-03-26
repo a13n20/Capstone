88335d15211d053492b676298bd1dadaceff37a4
a13n20 <nhoffmann3@csuchico.edu>
2025-03-26 10:02:59 -0700

    deployment setup

---
+++ download_files.sh
@@ -0,0 +1,6 @@
+echo "Downloading large files from Google Drive..."
+
+python3 -m gdown --id 118HRJ9LYgGDziW8arr9gpU9qyslLTWod -O api/phishing_detection/phish_detect_V1/model.safetensors
+python3 -m gdown --id 1hGnNFSVOk3tTe1Xv8T-kpbvbWe84j4yX -O api/password_aid/rockyou.txt 
+
+echo "Download complete"
