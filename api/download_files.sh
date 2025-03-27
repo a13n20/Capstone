5c7eebd3498c365f7f1e4de18f182ac301812bcb
a13n20 <nhoffmann3@csuchico.edu>
2025-03-27 11:41:25 -0700

    still trying to deploy

--- api/download_files.sh
+++ api/download_files.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo "Downloading large files from Google Drive..."
 
 python3 -m gdown --id 118HRJ9LYgGDziW8arr9gpU9qyslLTWod -O api/phishing_detection/phish_detect_V1/model.safetensors
