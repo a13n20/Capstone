eb040bef1a675095c7bf12970ed9b648e3cd105a
a13n20 <nhoffmann3@csuchico.edu>
2025-03-27 12:47:11 -0700

    fix file path....yet again

--- api/download_files.sh
+++ api/download_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Downloading large files from Google Drive..."
 
-python3 -m gdown --id 118HRJ9LYgGDziW8arr9gpU9qyslLTWod -O api/phishing_detection/phish_detect_V1/model.safetensors
-python3 -m gdown --id 1hGnNFSVOk3tTe1Xv8T-kpbvbWe84j4yX -O api/password_aid/rockyou.txt 
+python3 -m gdown --id 118HRJ9LYgGDziW8arr9gpU9qyslLTWod -O phishing_detection/phish_detect_V1/model.safetensors
+python3 -m gdown --id 1hGnNFSVOk3tTe1Xv8T-kpbvbWe84j4yX -O password_aid/rockyou.txt 
 
 echo "Download complete"
